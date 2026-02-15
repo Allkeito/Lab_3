947daccfe5ef5982b8c8b38e0def3f05224a95f6
Allkeito <rospikachu@gmail.com>
2026-02-15 21:39:30 +0300

    ЛабораторнаяРабота3_Упражнение3

---
+++ Lab_3_3_WinButNum/WinButNum/WinButNum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinButNum
+{
+    
+    public class ClickButton : Button
+    {
+        int mClicks = 0;
+
+        public int Clicks
+        {
+            get { return mClicks; }
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            mClicks++;
+            this.Invalidate(); 
+            base.OnClick(e);
+        }
+
+        
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            Graphics g = pevent.Graphics;
+
+            string text = Clicks.ToString();
+            
+            SizeF stringsize = g.MeasureString(text, this.Font);
+
+            g.DrawString(text, this.Font,
+                SystemBrushes.ControlText,
+                this.Width - stringsize.Width - 3,
+                this.Height - stringsize.Height - 3);
+        }
+    }
+}
