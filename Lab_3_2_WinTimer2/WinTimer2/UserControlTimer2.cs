dc26450859b7144bf1477fe5466bfa9614f813f6
Allkeito <rospikachu@gmail.com>
2026-02-15 21:27:51 +0300

    ЛабораторнаяРабота3_Упражнение2

---
+++ Lab_3_2_WinTimer2/WinTimer2/UserControlTimer2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinTimer2
+{
+    public partial class UserControlTimer2 : Control
+    {
+        public UserControlTimer2()
+        {
+            InitializeComponent();
+        }
+
+       
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+          
+            pe.Graphics.FillRectangle(Brushes.Blue, 0, 0, this.Width, this.Height);
+          
+            pe.Graphics.DrawString(DateTime.Now.ToLongTimeString(),
+                this.Font, new SolidBrush(this.ForeColor), 0, 0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Refresh(); 
+        }
+    }
+}
