fd16701b66fe370493d995e18d9361a6bb64daa8
Allkeito <rospikachu@gmail.com>
2026-02-15 21:10:34 +0300

    ЛабораторнаяРабота3_Упражнение1

---
+++ Lab_3_1_MyTimerLibrary/MyTimerLibrary/UserControl1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace MyTimerLibrary
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControlTimer1 = new MyTimerLibrary.UserControlTimer();
+            this.SuspendLayout();
+            // 
+            // userControlTimer1
+            // 
+            this.userControlTimer1.Location = new System.Drawing.Point(29, 27);
+            this.userControlTimer1.Name = "userControlTimer1";
+            this.userControlTimer1.Size = new System.Drawing.Size(89, 36);
+            this.userControlTimer1.TabIndex = 0;
+            this.userControlTimer1.TimeEnabled = true;
+            this.userControlTimer1.Load += new System.EventHandler(this.userControlTimer1_Load);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.userControlTimer1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControlTimer userControlTimer1;
+    }
+}
---
+++ Lab_3_1_MyTimerLibrary/MyTimerLibrary/UserControlTimer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyTimerLibrary
+{
+    public partial class UserControlTimer : UserControl
+    {
+        public UserControlTimer()
+        {
+            InitializeComponent();
+        }
+
+        public bool TimeEnabled
+        {
+            get { return timer1.Enabled; }
+            set { timer1.Enabled = value; }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+        } 
+    }
+}
